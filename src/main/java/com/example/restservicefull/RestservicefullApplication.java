edba4b37f1d8dac71609d0255d7934fa2911d299
Darren Chan <dfchan@idsdatanet.com>
2023-05-25 20:26:07 +0800

    saving dependency

--- src/main/java/com/example/restservicefull/RestservicefullApplication.java
+++ src/main/java/com/example/restservicefull/RestservicefullApplication.java
@@ -1,13 +1,13 @@
 package com.example.restservicefull;
 
+import org.quartz.SchedulerException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestservicefullApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestservicefullApplication.class, args);
-	}
-
+  public static void main(String[] args) throws SchedulerException {
+    SpringApplication.run(RestservicefullApplication.class, args);
+  }
 }
